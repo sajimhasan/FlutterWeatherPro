71ca7bc13cf6bccaf4cd38de5bbda4f773cbc640
sajimhasan <sim25642@gmail.com>
2025-01-05 11:46:47 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_01/weather.dart';
+
+void main(){
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Weather() ,
+    );
+  }
+}
