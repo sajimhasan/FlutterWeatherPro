500d31c7ed6d137df5f794928fd07f5fe21b0c24
sajimhasan <sim25642@gmail.com>
2025-01-07 12:27:11 +0600

    filter and image

--- lib/weather.dart
+++ lib/weather.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Weather extends StatefulWidget {
   const Weather({super.key});
@@ -11,6 +14,96 @@ class _WeatherState extends State<Weather> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
+      extendBody: true,
+      appBar: AppBar(
+       backgroundColor: Colors.white,
+       elevation:0,
+       systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.dark
+       ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent,
+                    shape: BoxShape.circle,
+
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(-3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent,
+                    shape: BoxShape.circle,
+
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0, -1.2),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    shape: BoxShape.rectangle,
+
+                  ),
+                ),
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 100,sigmaY: 100),
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.transparent,),
+                ),
+                 ),
+                 SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('📍Aix-en-Provence',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                      ),),
+                      SizedBox(height: 10,),
+                      Text('Good Morning',
+                      style: TextStyle(
+                        fontSize: 27,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                      ),
+                      ),
+                      Center(
+                        child: Image.asset("images/1.png")
+                        )
+                      
+
+                    ],
+                  ),
+                 ),
+                
+            ],
+          ),
+        ),
+      ),
+     
       
     );
   }
